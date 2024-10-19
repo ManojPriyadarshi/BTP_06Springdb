a201cd776f42ab7397c993dab882d8a4a75b3cd7
Manoj Priyadarshi <manojpriyadarshi@yahoo.com>
2024-10-19 09:21:53 +0530

    first change

--- 06springbtpdb_m/src/main/java/com/manoj/Application.java
+++ 06springbtpdb_m/src/main/java/com/manoj/Application.java
@@ -2,7 +2,7 @@ package com.manoj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Git change tracking new change
 @SpringBootApplication
 public class Application {
 
