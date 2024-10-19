6b9a5f2d6bdc3cc811e3c9d6feb012c3fe70ca70
Manoj Priyadarshi <manojpriyadarshi@yahoo.com>
2024-10-19 16:13:31 +0530

    corrected issue in pom.xml which was in first commit project. this is tested and running fine

--- 06springbtpdb_m/src/main/java/com/manoj/Application.java
+++ 06springbtpdb_m/src/main/java/com/manoj/Application.java
@@ -2,8 +2,11 @@ package com.manoj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Git change tracking new change
+//Git change tracking new change 1
+
+//Git change tracking new change 2
 @SpringBootApplication
+
 public class Application {
 
 	public static void main(String[] args) {
